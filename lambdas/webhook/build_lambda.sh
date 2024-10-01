6164b9716ff580f53af014678fcada4f5c13d2eb
Rey Williams <reynaldomwilliams@gmail.com>
2024-09-30 20:36:19 -0700

    add go mod tidy to build script

--- lambdas/webhook/build_lambda.sh
+++ lambdas/webhook/build_lambda.sh
@@ -1,6 +1,9 @@
 # Context on these commands can be found here:
 # https://docs.aws.amazon.com/lambda/latest/dg/golang-package.html#golang-package-mac-linux
 
+# Download go modules
+go mod tidy
+
 # compiles go program
 GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o archive/bootstrap main.go 
 
