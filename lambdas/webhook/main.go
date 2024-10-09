d0fdce9e79577461774c3d5c900805dd32275c65
Rey Williams <reynaldomwilliams@gmail.com>
2024-10-09 00:17:21 -0700

    update github go to version with needed types

--- lambdas/webhook/main.go
+++ lambdas/webhook/main.go
@@ -7,76 +7,40 @@ import (
 	"webhook/logger"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/google/go-github/v65/github"
+	github "github.com/google/go-github/v66/github"
 )
 
 var log logger.Logger
 
-// This type is introduced in a PR but has not yet been included in the latest version
-// https://github.com/google/go-github/pull/3254
-// TODO: bump go-github version and use included type (when v66 is out)
-type DeploymentReviewEvent struct {
-	// The action performed. Possible values are: "requested", "approved", or "rejected".
-	Action *string `json:"action,omitempty"`
+type event interface{}
 
-	// The following will be populated only if requested.
-	Requester   *github.User `json:"requester,omitempty"`
-	Environment *string      `json:"environment,omitempty"`
+func HandleRequest(ctx context.Context, event event) (*string, error) {
+	log.INFO("lambda handler execution has begun")
 
-	// The following will be populated only if approved or rejected.
-	Approver        *github.User      `json:"approver,omitempty"`
-	Comment         *string           `json:"comment,omitempty"`
-	WorkflowJobRuns []*WorkflowJobRun `json:"workflow_job_runs,omitempty"`
+	log.INFO("Consumed event: %+v\n", event)
 
-	Enterprise     *github.Enterprise         `json:"enterprise,omitempty"`
-	Installation   *github.Installation       `json:"installation,omitempty"`
-	Organization   *github.Organization       `json:"organization,omitempty"`
-	Repo           *github.Repository         `json:"repository,omitempty"`
-	Reviewers      []*github.RequiredReviewer `json:"reviewers,omitempty"`
-	Sender         *github.User               `json:"sender,omitempty"`
-	Since          *string                    `json:"since,omitempty"`
-	WorkflowJobRun *WorkflowJobRun            `json:"workflow_job_run,omitempty"`
-	WorkflowRun    *github.WorkflowRun        `json:"workflow_run,omitempty"`
-}
+	switch githubEvent := event.(type) {
+	case *github.DeploymentReviewEvent:
+		log.INFO("Event if of type: %T", event)
+		message := fmt.Sprintf("User %s has requested a review for %s environment in %s repo!", *githubEvent.Requester.Name, *githubEvent.Environment, *githubEvent.Repo.Name)
+		log.INFO("Constructed message %s", message)
+		return &message, nil
+	default:
+		log.INFO("Switched to default case - event if of type: %T", event)
+		// try to get json from event and log event.
+		eventJSON, err := json.Marshal(event)
+		if err != nil {
+			log.ERROR("failed to marshal event: %s", err.Error())
+			return nil, fmt.Errorf("failed to marshal event: %w", err)
+		}
 
-type WorkflowJobRun struct {
-	Conclusion  *string           `json:"conclusion,omitempty"`
-	CreatedAt   *github.Timestamp `json:"created_at,omitempty"`
-	Environment *string           `json:"environment,omitempty"`
-	HTMLURL     *string           `json:"html_url,omitempty"`
-	ID          *int64            `json:"id,omitempty"`
-	Name        *string           `json:"name,omitempty"`
-	Status      *string           `json:"status,omitempty"`
-	UpdatedAt   *github.Timestamp `json:"updated_at,omitempty"`
-}
-
-type Event interface{}
-
-func HandleRequest(ctx context.Context, event Event) (*string, error) {
-
-	// attempt to assert that this is a DeploymentReviewEvent
-	deploymentEvent, ok := event.(*DeploymentReviewEvent)
-	if !ok {
-		return nil, fmt.Errorf("unexpected event type: %T", event)
+		eventJSONString := string(eventJSON)
+		log.INFO("Received event: %s", string(eventJSONString))
+		return &eventJSONString, nil
 	}
-	// ensure the DeploymentReviewEvent is not null/nil
-	if deploymentEvent == nil {
-		return nil, fmt.Errorf("received nil event")
-	}
-
-	// try to get json from event and log event.
-	eventJSON, err := json.Marshal(event)
-	if err != nil {
-		log.ERROR("failed to marshal event: %s", err.Error())
-		return nil, fmt.Errorf("failed to marshal event: %w", err)
-	}
-	log.INFO("Received event: %s", string(eventJSON))
-
-	message := fmt.Sprintf("User %s has requested a review for %s environment in %s repo!", *deploymentEvent.Requester.Name, *deploymentEvent.Environment, *deploymentEvent.Repo.Name)
-	log.INFO("Constructed message %s", message)
-	return &message, nil
 }
 
 func main() {
+	log.INFO("main function called")
 	lambda.Start(HandleRequest)
 }
