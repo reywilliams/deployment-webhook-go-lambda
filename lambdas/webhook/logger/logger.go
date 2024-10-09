bd371254f537bcc8d438962864189acfe28fc336
Rey Williams <reynaldomwilliams@gmail.com>
2024-10-09 00:18:08 -0700

    fix functiin type and remove filename and line number for log messages

--- lambdas/webhook/logger/logger.go
+++ lambdas/webhook/logger/logger.go
@@ -24,23 +24,23 @@ once
 */
 func (l *Logger) initialize() {
 	once.Do(func() {
-        /**
-        This sets the following flags on a the logger
-        LUTC -> sets date and time to be in UTC
-        Ldate -> enables data 
-        Ltime -> enables time
-        Lshortfile -> enables short filename and line number
+		/**
+		  This sets the following flags on a the logger
+		  LUTC -> sets date and time to be in UTC
+		  Ldate -> enables data
+		  Ltime -> enables time
+		  Lshortfile -> enables short filename and line number
 
-        Resulting log message looks something like the following:
-        2024/10/05 01:02:03 main:35: <log_message>
-        **/
-		log.SetFlags(log.LUTC | log.Ldate | log.Ltime | log.Lshortfile)
+		  Resulting log message looks something like the following:
+		  2024/10/05 01:02:03 <PREFIX> <log_message>
+		  **/
+		log.SetFlags(log.LUTC | log.Ldate | log.Ltime | log.Lmsgprefix)
 		log.SetPrefix("")
 	})
 }
 
 // logs a message with a specified <prefix>
-func (l Logger) PREFIX(prefix string, format string, args ...interface{}) {
+func (l *Logger) PREFIX(prefix string, format string, args ...interface{}) {
 	l.initialize()
 	log.SetPrefix(prefix)
 	log.Printf(format, args...)
@@ -48,25 +48,25 @@ func (l Logger) PREFIX(prefix string, format string, args ...interface{}) {
 }
 
 // logs a message with an INFO prefix
-func (l Logger) INFO(format string, args ...interface{}) {
+func (l *Logger) INFO(format string, args ...interface{}) {
 	l.initialize()
-	log.SetPrefix("INFO")
+	log.SetPrefix("INFO: ")
 	log.Printf(format, args...)
 	log.SetPrefix("")
 }
 
 // logs a message with an WARN prefix
-func (l Logger) WARN(format string, args ...interface{}) {
+func (l *Logger) WARN(format string, args ...interface{}) {
 	l.initialize()
-	log.SetPrefix("WARN")
+	log.SetPrefix("WARN: ")
 	log.Printf(format, args...)
 	log.SetPrefix("")
 }
 
-// logs a message with an WARN prefix
-func (l Logger) ERROR(format string, args ...interface{}) {
+// logs a message with an ERROR prefix and return the error
+func (l *Logger) ERROR(format string, args ...interface{}) {
 	l.initialize()
-	log.SetPrefix("ERROR")
+	log.SetPrefix("ERROR: ")
 	log.Printf(format, args...)
 	log.SetPrefix("")
 }
