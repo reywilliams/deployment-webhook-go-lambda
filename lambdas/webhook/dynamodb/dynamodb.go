8a9c39d2ad67e80dd6ca85e42c194a73c6d07eb8
Rey Williams <reynaldomwilliams@gmail.com>
2024-10-09 22:58:46 -0700

    add function to check if user is in table with proper perms

--- lambdas/webhook/dynamodb/dynamodb.go
+++ lambdas/webhook/dynamodb/dynamodb.go
@@ -2,32 +2,73 @@ package dynamodb
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"regexp"
+	"strings"
 	"sync"
 	"webhook/logger"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-const AWS_REGION_DEFAULT = "us-west-2"
-const AWS_REGION_ENV_VAR_KEY = "AWS_REGION"
+var (
+	DYNAMO_DB_TABLE_NAME   string
+	AWS_REGION             string
+	dynamoDbClientInstance *dynamodb.Client
 
-const DYNAMO_DB_TABLE_NAME_ENV_VAR_KEY = "DYNAMO_DB_TABLE_NAME"
-const DYNAMO_DB_TABLE_NAME_DEFAULT = "deployment-webhooks-table"
+	log logger.Logger
 
-var AWS_REGION string
-var DYNAMO_DB_TABLE_NAME string
+	once sync.Once
+)
 
-var log logger.Logger
+const (
+	DYNAMO_DB_TABLE_NAME_ENV_VAR_KEY = "DYNAMO_DB_TABLE_NAME"
+	DYNAMO_DB_TABLE_NAME_DEFAULT     = "deployment-webhooks-table"
 
-var once sync.Once
+	AWS_REGION_ENV_VAR_KEY = "AWS_REGION"
+	AWS_REGION_DEFAULT     = "us-west-2"
+)
 
-var dynamoDbClientInstance *dynamodb.Client
+func init() {
+	source_aws_region()           // sources AWS_REGION env. variable
+	source_dynamo_db_table_name() // sources DYNAMO_DB_TABLE_NAME env. variable
+}
 
-func GetDynamoClient(ctx context.Context) (*dynamodb.Client, error) {
+func UserHasPermission(ctx context.Context, userEmail string, repository string, environment string) (*bool, error) {
+	log.INFO("UserHasPermission() method - inputs: userEmail: %s, repository %s", userEmail, repository)
 
+	partitionKey := userEmail // (hash_key in tf)
+	sortKey := strings.Join([]string{repository, environment}, "#")
+
+	dynamodbClient, err := getDynamoClient(ctx)
+	if err != nil {
+		log.ERROR("error observed while trying to get dynamodb client")
+		return nil, err
+	}
+
+	input := dynamodb.GetItemInput{
+		TableName: &DYNAMO_DB_TABLE_NAME,
+		Key: map[string]types.AttributeValue{
+			"email":    &types.AttributeValueMemberS{Value: partitionKey},
+			"repo-env": &types.AttributeValueMemberS{Value: sortKey},
+		},
+	}
+
+	result, err := dynamodbClient.GetItem(ctx, &input)
+	if err != nil {
+		log.ERROR("observed an error while trying to get entry from %s dynamodb table - PK: %s, SK: %s", DYNAMO_DB_TABLE_NAME, partitionKey, sortKey)
+		return nil, fmt.Errorf("observed an error while trying to get entry from %s dynamodb table - (PK: %s, SK: %s): %w", DYNAMO_DB_TABLE_NAME, sortKey, sortKey, err)
+	}
+
+	userHasPerm := (result.Item != nil)
+
+	return &userHasPerm, nil
+}
+
+func getDynamoClient(ctx context.Context) (*dynamodb.Client, error) {
 	if err := configureDynamoDbClient(ctx); err != nil {
 		return nil, err
 	}
@@ -36,11 +77,9 @@ func GetDynamoClient(ctx context.Context) (*dynamodb.Client, error) {
 
 func configureDynamoDbClient(ctx context.Context) error {
 	var err error
+
 	// ensures only one dynamodb client instance is created
 	once.Do(func() {
-		aws_region()           // sets global AWS_REGION var
-		dynamo_db_table_name() // sets global DYNAMO_DB_TABLE_NAME var
-
 		cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(AWS_REGION))
 		if err != nil {
 			log.ERROR("unable to load SDK config, %s", err.Error())
@@ -57,7 +96,7 @@ Retrieves AWS_REGION environment variable, sets global var (AWS_REGION) to:
 -> AWS_REGION env var value if it is not empty and valid (per regex pattern)
 -> AWS_REGION_DEFAULT if the above conditions are not met
 */
-func aws_region() {
+func source_aws_region() {
 	envSourcedRegion := os.Getenv(AWS_REGION_ENV_VAR_KEY)
 	log.INFO("AWS region environment variable (%s) retrieved with value: %s", AWS_REGION_ENV_VAR_KEY, envSourcedRegion)
 
@@ -77,7 +116,7 @@ Retrieves DYNAMO_DB_TABLE_NAME environment variable, sets global var (DYNAMO_DB_
 -> DYNAMO_DB_TABLE_NAME env var value if it is not empty
 -> DYNAMO_DB_TABLE_NAME_DEFAULT if empty
 */
-func dynamo_db_table_name() {
+func source_dynamo_db_table_name() {
 	envSourcedTableName := os.Getenv(DYNAMO_DB_TABLE_NAME_ENV_VAR_KEY)
 	log.INFO("DynamoDB Table Name environment variable (%s) retrieved with value: %s", DYNAMO_DB_TABLE_NAME_ENV_VAR_KEY, envSourcedTableName)
 
@@ -89,6 +128,11 @@ func dynamo_db_table_name() {
 	}
 }
 
+/*
+*
+Uses regex to determine if AWS region string
+is valid
+*/
 func isValidAWSRegion(region string) bool {
 	/**
 	Regex pattern for AWS regions
