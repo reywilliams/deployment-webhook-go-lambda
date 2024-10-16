0f403b1f634ef6e1966e37161644803e09e4ce19
Rey Williams <reynaldomwilliams@gmail.com>
2024-10-15 18:56:02 -0700

    add source IP policy for api gateway

---
+++ terraform/modules/api-gateway/source-ip-policy.tf
@@ -0,0 +1,35 @@
+
+data "external" "github_webhook_ips" {
+    program = ["bash", "-c", "curl -s https://api.github.com/meta | jq -r '.hooks | to_entries | map({(.key | tostring): .value}) | add'"]
+}
+
+# restricts API Gateway to source IPs from Github 
+# specifically the ones used for hooks 
+# see http://api.github.com/meta and look at "hooks"
+data "aws_iam_policy_document" "only_github_hook_ips_policy" {
+  statement {
+    effect    = "Deny"
+    actions   = ["execute-api:Invoke"]
+    resources = [aws_api_gateway_rest_api.webhook.execution_arn]
+
+
+    condition {
+      test     = "NotIpAddress"
+      variable = "aws:SourceIp"
+      values   = [for ip in data.external.github_webhook_ips.result : ip]
+    }
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+
+resource "aws_api_gateway_rest_api_policy" "only_github_hook_ips" {
+  rest_api_id = aws_api_gateway_rest_api.webhook.id
+
+  policy = data.aws_iam_policy_document.only_github_hook_ips_policy.json
+}
+
