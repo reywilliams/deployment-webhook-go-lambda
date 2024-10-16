861a00f3558f83e94a41aec7d6964e9bfe7ba992
Rey Williams <reynaldomwilliams@gmail.com>
2024-10-15 21:43:46 -0700

    update logs to be contextualized and add function to approve deployment if requester has access

--- lambdas/webhook/src/handlers/deployment_review.go
+++ lambdas/webhook/src/handlers/deployment_review.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/http"
 	"strings"
 	"sync"
 	db "webhook/dynamodb"
+	gh "webhook/github"
 	"webhook/logger"
 	"webhook/util"
 
@@ -32,22 +34,19 @@ func init() {
 }
 
 func RequesterHasPermission(ctx context.Context, requester string, repository string, environment string) (*bool, error) {
-	log.Infoln("checking if requester has permission", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
+	localLogger := log.With(zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
 
-	if util.AnyStringsEmpty(requester, repository, environment) {
-		log.Infoln("an input string was empty", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
-		return nil, errors.New("an input was string was empty")
-	}
+	localLogger.Infoln("checking if requester has permission")
 
 	dynamodbClient, err := db.GetDynamoClient(ctx)
 	if err != nil {
-		log.Errorln("error observed while trying to get dynamodb client", zap.Error(err))
+		localLogger.Errorln("error observed while trying to get dynamodb client", zap.Error(err))
 		return nil, err
 	}
 
 	hasAccess, err := checkRequesterAccess(ctx, dynamodbClient, requester, repository, environment)
 	if err != nil {
-		log.Errorln("error observed while checking request access", zap.Error(err))
+		localLogger.Errorln("error observed while checking request access", zap.Error(err))
 		return nil, err
 	}
 
@@ -55,37 +54,48 @@ func RequesterHasPermission(ctx context.Context, requester string, repository st
 }
 
 func HandleDeploymentReviewEvent(ctx context.Context, mocking bool, event *github.DeploymentReviewEvent) error {
-	log.Infof("Processing event: %T", event)
+	// we only handle request review events
+	if event.GetAction() != "requested" {
+		log.Debug("deployment review event was not for a request", zap.String("action", event.GetAction()))
+		return nil
+	}
 
 	requester := event.Requester.GetEmail()
 	repository := event.Repo.GetName()
 	environment := event.GetEnvironment()
 
+	localLogger := log.With(zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
+
+	localLogger.Infof("Processing event: %T", event)
+
+	if util.AnyStringsEmpty(requester, repository, environment) {
+		localLogger.Infoln("an input string was empty")
+		return errors.New("an input was string was empty")
+	}
+
 	if mocking {
-		var message string
-
-		if util.AnyStringsEmpty(requester, repository, environment) {
-			log.Infoln("an input string was empty, using fall back message for deployment_review event")
-			message = "fall back message as there were null pointers."
-		} else {
-			message = fmt.Sprintf("requester %s has needs a review for %s environment in %s repo!", *event.Requester.Name, *event.Environment, *event.Repo.Name)
-		}
-		log.Infof("constructed message: %s", message)
-
+		message := fmt.Sprintf("requester %s has needs a review for %s environment in %s repo!", requester, environment, repository)
+		localLogger.Infof("constructed message: %s", message)
 		return nil
 	} else {
 		requesterHasPerm, err := RequesterHasPermission(ctx, requester, repository, environment)
 		if err != nil {
-			log.Errorln("error observed while checking if requester has permission", zap.Error(err))
+			localLogger.Errorln("error observed while checking if requester has permission", zap.Error(err))
 			return err
 		}
 
 		if requesterHasPerm != nil && *requesterHasPerm {
-			log.Info("requester has permission", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
+			localLogger.Info("requester has permission, will attempt to approve deployments")
+
+			err := approveDeploymentReview(ctx, event)
+			if err != nil {
+				localLogger.Error("error observed while trying to approve deployment review", zap.Error(err))
+			}
 		}
+
+		// no error yielded, return nil
 		return nil
 	}
-
 }
 
 /*
@@ -97,6 +107,8 @@ Org access -> requester has access to a org, so all repos and all environments (
 *
 */
 func checkRequesterAccess(ctx context.Context, client *dynamodb.Client, requester string, repository string, environment string) (*bool, error) {
+
+	localLogger := log.With(zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
 
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel() // used to cancel other checks once access is found
@@ -120,11 +132,11 @@ func checkRequesterAccess(ctx context.Context, client *dynamodb.Client, requeste
 		requesterHasExactAccess, err := checkAccessByInput(ctx, exactAccessInput, client)
 		if err != nil {
 			errChan <- err
-			log.Errorln("error observed while trying to check if requester has exact access", zap.Error(err))
+			localLogger.Errorln("error observed while trying to check if requester has exact access", zap.Error(err))
 			return
 		}
 		if requesterHasExactAccess != nil && *requesterHasExactAccess {
-			log.Infoln("requester has exact access", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
+			localLogger.Infoln("requester has exact access", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
 			accessCheck <- requesterHasExactAccess
 			cancel()
 		}
@@ -145,11 +157,11 @@ func checkRequesterAccess(ctx context.Context, client *dynamodb.Client, requeste
 		requesterHasRepoAccess, err := checkAccessByInput(ctx, repoAccessInput, client)
 		if err != nil {
 			errChan <- err
-			log.Errorln("error observed while trying to check if requester has repo level access", zap.Error(err))
+			localLogger.Errorln("error observed while trying to check if requester has repo level access", zap.Error(err))
 			return
 		}
 		if requesterHasRepoAccess != nil && *requesterHasRepoAccess {
-			log.Infoln("requester has repo access", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
+			localLogger.Infoln("requester has repo access", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
 			accessCheck <- requesterHasRepoAccess
 			cancel()
 		}
@@ -169,11 +181,11 @@ func checkRequesterAccess(ctx context.Context, client *dynamodb.Client, requeste
 		requesterHasOrgAccess, err := checkAccessByInput(ctx, orgAccessInput, client)
 		if err != nil {
 			errChan <- err
-			log.Errorln("error observed while trying to check if requester has org access", zap.Error(err))
+			localLogger.Errorln("error observed while trying to check if requester has org access", zap.Error(err))
 			return
 		}
 		if requesterHasOrgAccess != nil && *requesterHasOrgAccess {
-			log.Infoln("requester has org access", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
+			localLogger.Infoln("requester has org access", zap.String("requester", requester), zap.String("repository", repository), zap.String("environment", environment))
 			accessCheck <- requesterHasOrgAccess
 			cancel()
 		}
@@ -210,13 +222,15 @@ func checkRequesterAccess(ctx context.Context, client *dynamodb.Client, requeste
 
 	for result := range accessCheck {
 		if result != nil && *result {
+			localLogger.Infoln("requester has access")
 			return result, nil
 		}
 	}
 
-	// user did not have any access, return false
-	userAccess := false
-	return &userAccess, nil
+	// requester did not have any access, return false
+	requesterAccess := false
+	localLogger.Infoln("requester did not have access")
+	return &requesterAccess, nil
 }
 
 func checkAccessByInput(ctx context.Context, input *dynamodb.GetItemInput, client *dynamodb.Client) (*bool, error) {
@@ -227,4 +241,51 @@ func checkAccessByInput(ctx context.Context, input *dynamodb.GetItemInput, clien
 	}
 	hasAccess := (result.Item != nil)
 	return &hasAccess, nil
+}
+
+/*
+*
+uses event attributes (owner, repo, runID) to get environment IDs and then use those
+environment IDs to approve all pending deployments
+*/
+func approveDeploymentReview(ctx context.Context, event *github.DeploymentReviewEvent) error {
+	ghClient := gh.GetGitHubClient()
+
+	owner := event.GetOrganization().GetName()
+	repo := event.GetRepo().GetName()
+	runID := event.WorkflowJobRun.GetID()
+
+	localLogger := log.With(zap.String("owner", owner), zap.String("repo", repo), zap.Int64("runID", runID))
+
+	if util.AnyStringsEmpty(owner, repo) || runID == 0 {
+		err := fmt.Errorf("event values continued zero values")
+		localLogger.Errorln("error observed while getting values to approve review", zap.Error(err))
+		return err
+	}
+
+	gotPendingDeployments, getPendDeployResp, gotPendingDeploymentsErr := ghClient.Actions.GetPendingDeployments(ctx, owner, repo, runID)
+	if getPendDeployResp.Response.StatusCode != http.StatusOK || gotPendingDeploymentsErr != nil {
+		localLogger.Errorln("error or incorrect status code observed while getting pending deployments", zap.Error(gotPendingDeploymentsErr), zap.Int("status_code", getPendDeployResp.Response.StatusCode))
+		return gotPendingDeploymentsErr
+	}
+
+	var envIDs []int64
+	for _, pendingDeployment := range gotPendingDeployments {
+		envIDs = append(envIDs, pendingDeployment.GetEnvironment().GetID())
+	}
+
+	approvedDeployments, approvalResp, approvalErr := ghClient.Actions.PendingDeployments(ctx, owner, repo, runID, &github.PendingDeploymentsRequest{EnvironmentIDs: envIDs, State: "approved", Comment: "Approved via Go GitHub Webhook Lambda! 🚀"})
+	if approvalResp.Response.StatusCode != http.StatusOK || approvalErr != nil {
+		localLogger.Error("error or incorrect status code observed while approving deployments", zap.Error(approvalErr), zap.Int("status_code", approvalResp.Response.StatusCode), zap.Int64s("env_IDs", envIDs))
+		return approvalErr
+	}
+
+	var approvedDeploymentsURLs []string
+
+	for _, approvedDeployment := range approvedDeployments {
+		approvedDeploymentsURLs = append(approvedDeploymentsURLs, approvedDeployment.GetURL())
+	}
+	localLogger.Infoln("approved deployments", zap.Strings("deployment_urls", approvedDeploymentsURLs))
+
+	return nil
 }
