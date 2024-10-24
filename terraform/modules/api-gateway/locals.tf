c2578786d521871bc47fe545245d7d335ca17d7d
Rey Williams <reynaldomwilliams@gmail.com>
2024-10-23 23:12:03 -0700

    update trigger files

--- terraform/modules/api-gateway/locals.tf
+++ terraform/modules/api-gateway/locals.tf
@@ -1,13 +1,9 @@
 locals {
   profile = "${var.project_name}-${var.environment}"
 
-  # these are the files that, if changed, will trigger a new api gateway deployment
-  gateway_trigger_files = [
-    "gateway.tf",
-    "integration.tf",
-    "deployment.tf",
-    "responses.tf",
-  ]
+  # list of all files in module; if changed, will trigger a new api gateway deployment
+  gateway_trigger_files = [for file in fileset(path.module, "*.tf") : tostring(file)]
+
   gateway_file_hashes         = { for file in local.gateway_trigger_files : file => md5(file(file)) }
   gateway_files_combined_hash = md5(join("", [for file in local.gateway_trigger_files : local.gateway_file_hashes[file]]))
 
